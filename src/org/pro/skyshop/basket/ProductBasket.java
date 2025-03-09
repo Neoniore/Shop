64772bb1d39185c2aa794224ebb7f889a9c21199
TabulaRasa <lforjust@gmail.com>
2025-03-09 20:53:30 +0300

    доделал метод добавления товара в корзину, добавил метод вывода стоимости всей корзины. добавил метод на проверку пустоты корзины

--- src/org/pro/skyshop/basket/ProductBasket.java
+++ src/org/pro/skyshop/basket/ProductBasket.java
@@ -2,21 +2,57 @@ package org.pro.skyshop.basket;
 
 import org.pro.skyshop.product.Product;
 
+import java.util.Arrays;
+
 public class ProductBasket {
     private static long id;
-    private static final int volumeOfTheBasket = 5; // объем корзины
+    private static final int capacityOfTheBasket = 5; // объем корзины
     private Product[] products;
 
     public ProductBasket() {
         id++;
-        products = new Product[volumeOfTheBasket];
+        products = new Product[capacityOfTheBasket];
     }
 
+    // добавление товара в корзину
     public void addingGoods(Product product) {
-
+        if (isBasketFull(products)) {
+            throw new IllegalStateException("Невозможно добавить продукт");
+        }
+        for (int i = 0; i < products.length - 1; i++) {
+            if (products[i] == null) {
+                products[i] = product;
+            }
+        }
     }
+
+    // получение стоимости всей корзины
+    public int obtainingTheCostOfTheBasket() {
+        if (isBasketEmpty(products)) {
+            throw new IllegalStateException("Корзина пуста, невозможно рассчитать сумму");
+        }
+        int theCostOfTheBasket = 0;
+        for (Product product : products) {
+            theCostOfTheBasket =+ product.getPrice();
+        }
+        return theCostOfTheBasket;
+    }
+
+    // печать содержимого корзины
+
 
     private boolean isBasketFull(Product[] products) {
-        return products != null && products[volumeOfTheBasket - 1] != null;
+        return products != null && products[capacityOfTheBasket - 1] != null;
+    }
+
+    private boolean isBasketEmpty(Product[] products) {
+        boolean isEmpty = true;
+        for (Product product : products) {
+            if (product != null) {
+                isEmpty = false;
+                break;
+            }
+        }
+        return isEmpty;
     }
 }
